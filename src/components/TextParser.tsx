441698f1be884ca5d8398884f270311c3ea97244
Jayson Reis <santosdosreis@gmail.com>
2018-06-28 20:22:51 +0200

    wip

--- src/components/TextParser.tsx
+++ src/components/TextParser.tsx
@@ -1,21 +1,49 @@
 import { Button, TextField } from "@material-ui/core";
 import * as React from 'react';
 
-class TextParser extends React.Component {
+interface ITextParserState {
+    text: string
+}
+
+interface IWordTreeRequest {
+    text: string;
+    pre_processor?: "ttml2";
+}
+
+export default class TextParser extends React.Component<{}, ITextParserState> {
     constructor(props: any, comp: any) {
         super(props, comp);
         this.state = {
-            text: ""
+            text: "Abc"
         };
     }
+
     public render() {
         return (
             <div>
-                <TextField multiline={true} value="ae" />
-                <Button>Parse text</Button>
+                <TextField multiline={true} value={this.state.text} onChange={this.handleChange} />
+                <Button onClick={this.handleSubmit}>Parse text</Button>
             </div>
         );
     }
-}
 
-export default TextParser;
+    private handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ text: e.target.value });
+    }
+
+    private handleWordTreeResponse = (response: Response) => {
+        const c = console;
+        c.log(response);
+    }
+
+    private handleSubmit = (e: any) => {
+        const payload: IWordTreeRequest = {
+            text: this.state.text,
+        };
+        const request: RequestInit = {
+            body: JSON.stringify(payload),
+            method: 'post'
+        };
+        fetch("http://localhost:5000/v1/build-word-tree/de", request).then(this.handleWordTreeResponse);
+    }
+}
